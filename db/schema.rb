2e21c96924971155df2c7b684d4383691a9af459
SethEPeters <speters32@academic.rrc.ca>
2026-07-30 19:53:38 -0500

    more work on cart and checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_19_220651) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_30_044721) do
   create_table "abouts", force: :cascade do |t|
     t.text "about_info"
     t.datetime "created_at", null: false
@@ -71,6 +71,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_19_220651) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -105,6 +110,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_19_220651) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
