d75606c76a897da38e074385ffe6de1a3c378252
SethEPeters <speters32@academic.rrc.ca>
2026-08-11 23:17:16 -0500

    start development on image file upload automatic scaling

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :category
-  has_one_attached :item_image_path
+  has_one_attached :item_image_path do |attachable|
+    attachable.variant :list_thumbnail, resize_to_limit: [200, 200], preprocessed: true
+    attachable.variant :item_view, resize_to_limit: [200, 200], preprocessed: true
+  end
 
   validates :title, presence: true
   validates :description, presence: true
